792ef53e03b791f603c463a6753b7db1a82af623
frasalvi <salvi.fnc@gmail.com>
2022-12-14 22:12:27 +0100

    defaults

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Run as:
-python main.py -n {YOUR_N} -beta {YOUR_BETA}
+python main.py -n {YOUR_N}
 """
 
 import argparse
@@ -11,23 +11,22 @@ from metropolis import run_metropolis
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--n", help="number of queens")
-    parser.add_argument("-beta", "--beta", help="initial beta")
     args = parser.parse_args()
     N = int(args.n) if args.n else 4
-    beta = float(args.beta) if args.beta else 0.01
-    max_moves = 1000000
+    max_moves = 1_000_000
 
     t = time()
     iters, queens, _ = run_metropolis(
         N,
         max_moves,
-        beta,
+        beta_init=20,
         mode="solve",
-        beta_strategy="annealing_quantized",
+        beta_strategy="fixed",
         strategy_params={"iterations_step": 1000, "annealing_factor": 2},
     )
+    print(queens)
 
-    if iters == -1:
+    if iters == max_moves-1:
         print(f"No solution found in {max_moves} moves.")
     else:
         print(f"Solution found in {iters} moves, {time() - t:e} seconds.")
