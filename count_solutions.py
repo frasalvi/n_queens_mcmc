1c8f879741169681d7d8f855439fa02c5ee482e6
frasalvi <salvi.fnc@gmail.com>
2022-12-14 22:12:06 +0100

    compute log(Z_beta)

--- count_solutions.py
+++ count_solutions.py
@@ -1,15 +1,22 @@
+"""
+Run as:
+python count_solutions.py -n {YOUR_N}
+"""
+
 import os
 import pickle
 import numpy as np
 import mpmath as mp
+import argparse
 from metropolis import run_metropolis
 
 mp.mp.dps = 300
 
 
-def count_solutions(N, convergence_moves, counting_moves, beta_grid):
+def get_ratios(N, convergence_moves, counting_moves, beta_grid, debug=True):
     max_moves = convergence_moves + counting_moves
     ratio_sum = 0
+    ratio_arr = []
 
     for i in range(len(beta_grid) - 2, -1, -1):
         beta_t1 = beta_grid[i + 1]
@@ -27,6 +34,7 @@ def count_solutions(N, convergence_moves, counting_moves, beta_grid):
                 beta_strategy="fixed",
                 strategy_params=None,
                 count_params={"convergence_moves": convergence_moves},
+                debug=debug
             )
             with open(f"data/{N}_beta{beta_t}.pkl", "wb") as f:
                 pickle.dump(conflict_dict, f)
@@ -41,44 +49,67 @@ def count_solutions(N, convergence_moves, counting_moves, beta_grid):
             )
             / counting_moves
         )
-        ratio_sum += mp.log(ratio)
+        # ratio_sum += mp.log(ratio)
+        ratio_arr.append(mp.mpmathify(ratio))
 
         with mp.workdps(4):
-            print(f"beta {beta_t}, ratio {ratio}")
+            debug and print(f"beta {beta_t}, ratio {ratio}")
 
-    with mp.workdps(4):
-        print(f"ratio_sum = {ratio_sum}")
-    Z0 = mp.factorial(N)
-    Z = Z0 * mp.exp(ratio_sum)
+    # with mp.workdps(4):
+    #     debug and print(f"ratio_sum = {ratio_sum}")
+    # Z0 = mp.factorial(N)
+    # Z = Z0 * mp.exp(ratio_sum)
+    # return Z
+    return ratio_arr
+
+
+def estimate_zetab(N, convergence_moves, counting_moves, beta_grid, debug=True):
+    ratio_arr = get_ratios(N, convergence_moves, counting_moves, beta_grid, debug)
+    T = len(ratio_arr)
+    Z_arr = np.empty(T+1, dtype=object)
+    Z_arr[0] = mp.factorial(N)
+    for i in range(1, T+1):
+        Z_arr[i] = Z_arr[i-1] * ratio_arr[T-i]
+    return Z_arr
+
+def count_solutions(N, convergence_moves, counting_moves, beta_grid, debug=True):
+    Z_arr = estimate_zetab(N, convergence_moves, counting_moves, beta_grid, debug)
+    Z = Z_arr[-1]
     return Z
 
 
 if __name__ == "__main__":
-    np.random.seed(42)
-    N = 1000
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--n", help="number of queens")
+    args = parser.parse_args()
+    N = int(args.n) if args.n else 1000
     convergence_moves = 200_000
     counting_moves = 800_000
-    beta_grid = [
-        0.0001,
-        0.01,
-        0.1,
-        0.25,
-        0.375,
-        0.5,
-        0.625,
-        0.75,
-        1,
-        1.25,
-        1.5,
-        1.75,
-        2,
-        2.5,
-        3,
-        4,
-        5,
-        10,
-        20,
-    ]
+
+    if N <= 20:
+        beta_grid = [0, 0.01, 1, 3, 5, 10]
+    else:
+        beta_grid = [
+            0,
+            0.01,
+            0.1,
+            0.25,
+            0.375,
+            0.5,
+            0.625,
+            0.75,
+            1,
+            1.25,
+            1.5,
+            1.75,
+            2,
+            2.5,
+            3,
+            4,
+            5,
+            10,
+            20,
+        ]
 
     Z = count_solutions(N, convergence_moves, counting_moves, beta_grid)
 
