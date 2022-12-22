26881f60966252f073e4774a15bbec8c5bc9aece
frasalvi <salvi.fnc@gmail.com>
2022-12-22 14:04:02 +0100

    progressive saving

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--n", help="number of queens")
     args = parser.parse_args()
     N = int(args.n) if args.n else 4
-    max_moves = 1_000_000
+    max_moves = 100_000_000
 
     t = time()
     iters, queens, _ = run_metropolis(
@@ -24,17 +24,10 @@ if __name__ == "__main__":
         beta_init=0.01,
         mode="solve",
         beta_strategy="annealing_quantized",
-        strategy_params={"iterations_step": 10000, "annealing_factor": 2},
+        strategy_params={"iterations_step": 5000, "annealing_factor": 2, "annealing_max": 15},
     )
 
     if iters == max_moves-1:
         print(f"No solution found in {max_moves} moves.")
     else:
         print(f"Solution found in {iters} moves, {time() - t:e} seconds.")
-
-    if DUMP:
-        filename = f"data/solution_{N}.csv"
-        with open(filename, 'w') as csv_file:
-            wr = csv.writer(csv_file, delimiter=',', lineterminator='\n')
-            for queen in queens:
-                wr.writerow(list(queen))
